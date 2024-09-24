1c5231e56f5dd25ad0f730edb6651833c3647551
Adam Biggs <wareaglebiggs@gmail.com>
2024-09-24 11:15:10 -0500

    Script setup

--- Assets/Scripts/PathGeneration.cs
+++ Assets/Scripts/PathGeneration.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class PathGeneration : MonoBehaviour
 {
+    public GameObject StartingPoint;
+    public GameObject EndPoint;
+
+    public List<Transform> Points;
+    
     // Start is called before the first frame update
     void Start()
     {
