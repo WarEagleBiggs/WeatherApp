fc1d4ed4668270a1bff8a305674c07e42916794e
Adam Biggs <wareaglebiggs@gmail.com>
2025-02-21 09:24:04 -0600

    Made hurricanes spin

---
+++ Assets/Scripts/ModelSpin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelSpin : MonoBehaviour
+{
+    public float spinRate;
+
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(0,0,1) * spinRate * Time.deltaTime);
+    }
+}
